55e71cfece4adf03e3a6af1ca9c439dbbeeee8cb
Brian Fontenot <fontenotbrian@gmail.com>
2014-01-12 16:53:52 -0600

    Initial Commit

---
+++ Sign Up In Animation Example/IOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IOSAppDelegate.h
+//  Sign Up In Animation Example
+//
+//  Created by Brian Fontenot on 1/12/14.
+//  Copyright (c) 2014 Brian Fontenot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sign Up In Animation Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sign Up In Animation Example
+//
+//  Created by Brian Fontenot on 1/12/14.
+//  Copyright (c) 2014 Brian Fontenot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IOSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IOSAppDelegate class]));
+    }
+}
